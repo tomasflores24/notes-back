8b3e7affc39612b53b30f1101d301167e65979b3
Tomas Flores <tomy24ssf@gmail.com>
2023-10-30 12:58:48 -0300

    add connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceConfig } from './config/data.source';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(DataSourceConfig)],
 })
 export class AppModule {}
