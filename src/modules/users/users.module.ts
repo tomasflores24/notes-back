0b15727cd38f0cc1603074642c2ef6aa5d1b82d2
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 10:39:04 -0300

    add user module (entity, controller and service)

---
+++ src/modules/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+import { UserEntity } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
