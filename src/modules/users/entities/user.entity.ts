9415262bd5cadef56d0eaf426c860db635a16cfa
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 18:01:05 -0300

    relation user note

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { StatusEntity } from '../../status/entitities/status.entity';
 import { IUser } from '../../../common/interfaces/user.interface';
 import { BaseEntity } from '../../../common/base-entities/base.entity';
+import { NoteEntity } from 'src/modules/notes/entities/note.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity extends BaseEntity implements IUser {
@@ -14,7 +15,10 @@ export class UserEntity extends BaseEntity implements IUser {
   @Column()
   password!: string;
 
-  @ManyToOne(() => StatusEntity, (status) => status.users, {})
+  @ManyToOne(() => StatusEntity, (status) => status.users)
   @JoinColumn({ name: 'status' })
   status!: StatusEntity;
+
+  @OneToMany(() => NoteEntity, (note) => note.user)
+  notes!: NoteEntity[];
 }
