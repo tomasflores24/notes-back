ec576481d23064b4519e0ea6b479e5b3cc922bae
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 18:00:02 -0300

    add module note

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { StatusModule } from './modules/status/status.module';
 import { DataSourceConfig } from './config/db.config';
+import { NotesModule } from './modules/notes/notes.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(DataSourceConfig), UsersModule, StatusModule],
+  imports: [
+    TypeOrmModule.forRoot(DataSourceConfig),
+    UsersModule,
+    StatusModule,
+    NotesModule,
+  ],
 })
 export class AppModule {}
