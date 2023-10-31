7fa8723c77c093005f1ec1ca5c4d254ec684d72b
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 11:00:48 -0300

    add module status

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
 import { UsersModule } from './modules/users/users.module';
+import { StatusModule } from './modules/status/status.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(DataSourceConfig), UsersModule],
+  imports: [TypeOrmModule.forRoot(DataSourceConfig), UsersModule, StatusModule],
 })
 export class AppModule {}
