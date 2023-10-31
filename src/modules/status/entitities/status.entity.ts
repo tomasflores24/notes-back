68d00cfc3843d89e4eb1b0e9fb8904394c72fb2d
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 11:47:23 -0300

    add base entity and implements

--- src/modules/status/entitities/status.entity.ts
+++ src/modules/status/entitities/status.entity.ts
@@ -1,9 +1,10 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { STATUS } from '../../../constants/enums/status.enum';
 import { UserEntity } from '../../users/entities/user.entity';
+import { IEntity } from 'src/interfaces/entity.interface';
 
 @Entity({ name: 'status' })
-export class StatusEntity {
+export class StatusEntity implements IEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
