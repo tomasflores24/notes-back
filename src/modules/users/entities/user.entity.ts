71eb2e8248da90043bd01b4cd39d4e483c6037db
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 18:56:19 -0300

    relation notes

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { StatusEntity } from '../../status/entitities/status.entity';
 import { IUser } from '../../../common/interfaces/user.interface';
 import { BaseEntity } from '../../../common/base-entities/base.entity';
-import { NoteEntity } from 'src/modules/notes/entities/note.entity';
+import { NoteEntity } from '../../../modules/notes/entities/note.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity extends BaseEntity implements IUser {
