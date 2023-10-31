ab93a7285c3d10099ee0ed38c1bae35eaee5b367
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 10:38:06 -0300

    inject users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
+import { UsersModule } from './modules/users/users.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(DataSourceConfig)],
+  imports: [TypeOrmModule.forRoot(DataSourceConfig), UsersModule],
 })
 export class AppModule {}
