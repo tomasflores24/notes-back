968c7e5fbfa87e0a2ccc05525cb2975a127f8e4e
Tomas Flores <tomy24ssf@gmail.com>
2023-10-31 18:00:44 -0300

    relation notes

--- src/modules/status/entitities/status.entity.ts
+++ src/modules/status/entitities/status.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { STATUS } from '../../../common/enums/status.enum';
 import { UserEntity } from '../../users/entities/user.entity';
 import { IEntity } from 'src/common/interfaces/entity.interface';
+import { NoteEntity } from 'src/modules/notes/entities/note.entity';
 
 @Entity({ name: 'status' })
 export class StatusEntity implements IEntity {
@@ -13,4 +14,7 @@ export class StatusEntity implements IEntity {
 
   @OneToMany(() => UserEntity, (user) => user.status)
   users: UserEntity[];
+
+  @OneToMany(() => NoteEntity, (note) => note.status)
+  notes: NoteEntity[];
 }
