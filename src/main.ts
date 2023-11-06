f4c69d3be706e4432edf7041310c94d36e0ead48
Tomas Flores <tomy24ssf@gmail.com>
2023-11-06 10:48:03 -0300

    Added global pipes, set global prefix, enable cors, added swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as morgan from 'morgan';
+import { CORS, configSwagger, ConfigValidationPipe } from './config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(ConfigValidationPipe);
+  app.setGlobalPrefix('api');
+  app.enableCors(CORS);
+  app.use(morgan('dev'));
+
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('doc', app, document);
+
   const PORT = process.env.APP_PORT || 3000;
   await app.listen(PORT);
 }
