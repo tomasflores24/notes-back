36e9d5b1980a57bdf5b40fbb052cd4bfced3afdb
Tomas Flores <tomy24ssf@gmail.com>
2023-11-06 18:01:41 -0300

    added role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { DataSourceConfig } from './config';
 import { UsersModule } from './modules/users/users.module';
 import { StatusModule } from './modules/status/status.module';
 import { NotesModule } from './modules/notes/notes.module';
+import { RolesModule } from './modules/roles/roles.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(DataSourceConfig),
     UsersModule,
     StatusModule,
     NotesModule,
+    RolesModule,
   ],
 })
 export class AppModule {}
