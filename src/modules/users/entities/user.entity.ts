1145ba85a7e064510c35f431e3c0798783e48b7a
Tomas Flores <tomy24ssf@gmail.com>
2023-11-06 18:02:42 -0300

    added relation user - role

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { StatusEntity } from '../../status/entitities/status.entity';
 import { IUser } from '../../../common/interfaces';
 import { BaseEntity } from '../../../common/base-entities/base.entity';
 import { NoteEntity } from '../../../modules/notes/entities/note.entity';
+import { RoleEntity } from '../../../modules/roles/entities/role.entity';
 
 @Entity({ name: 'users' })
 export class UserEntity extends BaseEntity implements IUser {
@@ -21,4 +22,8 @@ export class UserEntity extends BaseEntity implements IUser {
 
   @OneToMany(() => NoteEntity, (note) => note.user)
   notes!: NoteEntity[];
+
+  @ManyToOne(() => RoleEntity, (role) => role.users)
+  @JoinColumn({ name: 'roles' })
+  role!: RoleEntity;
 }
