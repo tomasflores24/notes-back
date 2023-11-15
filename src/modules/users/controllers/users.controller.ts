6c83c47602f0eb5e4f8cb25e2f78ebf150dc4296
Tomas Flores <tomy24ssf@gmail.com>
2023-11-14 21:17:45 -0300

    added new docorators

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -6,26 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { PublicAccess } from 'src/modules/decorators/public.decorator';
+import { AdminAccess } from 'src/modules/decorators/admin.decorator';
 
 @Controller('users')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @PublicAccess()
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @AdminAccess()
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
