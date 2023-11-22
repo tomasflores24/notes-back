9b28fdd7b7c5ac480ba5a873d29bdaa17061c06f
Tomas Flores <tomy24ssf@gmail.com>
2023-11-22 17:36:35 -0300

    added usersmodule

--- src/modules/notes/notes.module.ts
+++ src/modules/notes/notes.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesService } from './services/notes.service';
 import { NotesController } from './controllers/notes.controller';
 import { NoteEntity } from './entities/note.entity';
+import { UsersService } from '../users/services/users.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([NoteEntity])],
+  imports: [TypeOrmModule.forFeature([NoteEntity]), UsersModule],
   controllers: [NotesController],
-  providers: [NotesService],
+  providers: [NotesService, UsersService],
 })
 export class NotesModule {}
