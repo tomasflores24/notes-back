80d45b5054fce0c142e87656431b73200fa3598b
Tomas Flores <tomy24ssf@gmail.com>
2023-12-06 11:47:24 -0300

    Refactor config swagger

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -2,7 +2,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const configSwagger = new DocumentBuilder()
   .setTitle('Notes')
-  .setDescription('Description')
+  .setDescription('')
   .setVersion('1.0')
-  .addTag('cats')
+  .addBearerAuth()
   .build();
